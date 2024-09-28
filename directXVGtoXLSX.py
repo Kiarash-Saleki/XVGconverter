46567a1c5ae849ef4e720dea5abb9074bd552552
Kiarash-Saleki <61684980+Kiarash-Saleki@users.noreply.github.com>
2024-09-28 13:59:09 +0330

    Create directXVGtoXLSX.py

---
+++ directXVGtoXLSX.py
@@ -0,0 +1,15 @@
+Enter the path to the .xvg file: input.xvg
+Which column do you want (X, Y)? Type 'end' to quit or 'save' to save to Excel: x
+[clears screen]
+X-axis values (separate column):
+0.0
+0.1
+0.2
+0.3
+
+Which column do you want (X, Y)? Type 'end' to quit or 'save' to save to Excel: save
+Enter the output Excel file name (e.g., 'output.xlsx'): output.xlsx
+Data successfully saved to output.xlsx
+
+Which column do you want (X, Y)? Type 'end' to quit or 'save' to save to Excel: end
+Exiting program.
